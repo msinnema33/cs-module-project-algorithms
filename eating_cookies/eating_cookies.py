e4f5b5ebf357f6e3b6f8756abe18b91325ee3f4d
Mike Sinnema <msinnema33@gmail.com>
2020-06-16 13:33:24 -0700

    cookies small works - error in large test

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,13 @@ Returns: an integer
 def eating_cookies(n):
     # Your code here
 
-    pass
+    arr = [1, 1, 2, 4] + ([0] * (n - 3))
+    if n < 4:
+        return arr[n]
+    else:
+        for i in range(4, n + 1):
+            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3]
+    return arr.pop()
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
