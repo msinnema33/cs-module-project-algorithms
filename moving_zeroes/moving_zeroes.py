6db71b91e3241e61893d66538c243a1aa334ac8a
Mike Sinnema <msinnema33@gmail.com>
2020-06-17 18:00:38 -0700

    moving zeros passes all but test 3 (a list with no zeros)

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,20 +2,47 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# need to check into the swap/no swap logic
+
 def moving_zeroes(arr):
-    # Your code here
+    # count = 0 
+    # for x in arr:
+    #     if x == 0:
+    #         count += 1
+    #         arr.remove(x)
+    #     # else:
+    #     #     return arr
+    #     return arr + ([0] * (count - 1))
+    #
+    #This passes all but a list without zeros
     i = 0
-    for i in arr:
-        # while i <= (len(arr) -1):
-        j = i+1
-        if arr[i] == 0:
-            arr[i], arr[j] = arr[j], arr[i]
-        else:
-            i += 1
+    while i < len(arr):
+        # if arr[i] != 0:
+        #     continue
+        arr.append(arr.pop(arr.index(0)))
+        i +=1
     return arr
 
-if __name__ == '__main__':
-    # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+         
 
-    print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+    if __name__ == '__main__': 
+    # Use the main function here to test out your implementation
+        arr = [0, 3, 1, 0, -2]
+
+        print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+
+
+# Your code here
+    # for x in range(0, (len(arr)-1)):
+    #     i = x
+    #     j = i+1
+    #     for z in range(1, (len(arr) - 2)):
+    #         if arr[i] == 0:
+    #             arr[i], arr[j] = arr[j], arr[i]
+    #             i += 1
+    #             j += 1
+    #         else:
+    #             i += 1
+    #             j += 1
+        
+    #     return arr
